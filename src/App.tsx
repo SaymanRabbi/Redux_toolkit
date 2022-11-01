92947e67266edb7cee40004af2ab768a76e51ce0
saymanrabbi <mdrabbi.asm@gmail.com>
2022-11-01 22:02:05 +0600

    increment

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { increment } from "./Store/Store";
 function App() {
+  const dispatch = useDispatch();
+  const counter = useSelector((state: any) => state.counter.counter);
   return (
     <div className="App">
       <h1>This is Redux with TypeScript</h1>
+      <h2>Counter: {counter}</h2>
+      <button onClick={() => dispatch(increment())}>Increment</button>
     </div>
   );
 }
