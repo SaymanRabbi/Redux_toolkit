893629bd133946833a75042ea8348c38d1d3074e
saymanrabbi <mdrabbi.asm@gmail.com>
2022-11-01 22:22:58 +0600

    decrement and addNew

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { increment } from "./Store/Store";
+import { add, decrement, increment } from "./Store/Store";
 function App() {
   const dispatch = useDispatch();
   const counter = useSelector((state: any) => state.counter.counter);
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+  const addNew = () => {
+    dispatch(add(10));
+  };
   return (
     <div className="App">
       <h1>This is Redux with TypeScript</h1>
       <h2>Counter: {counter}</h2>
-      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => handleIncrement()}>Increment</button>
+      <button onClick={() => handleDecrement()}>Decrement</button>
+      <button onClick={() => addNew()}>Add By 10</button>
     </div>
   );
 }
